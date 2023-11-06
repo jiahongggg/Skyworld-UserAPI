0f37f7e79246b46c94b303871713336c0fc8990f
jiahongg <88899688+jiahongg@users.noreply.github.com>
2023-11-06 16:41:22 +0800

    fix error

--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ async function connect() {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE_NAME
+    database: process.env.MYSQL_DATABASE
   });
 }
 
