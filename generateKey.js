ef98a96c6a9ff8684a7f4baf3da626c3cad64697
jiahongg <88899688+jiahongg@users.noreply.github.com>
2023-12-07 06:06:36 +0800

    refactor

--- generateKey.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a secure random string for JWT secret key
-const jwtSecret = crypto.randomBytes(32).toString('hex');
-console.log('JWT Secret Key:', jwtSecret);
