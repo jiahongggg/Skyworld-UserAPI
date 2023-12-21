23ee14fda02a5a8e97274d4352a16d58c86e1594
jiahongg <88899688+jiahongg@users.noreply.github.com>
2023-12-22 06:26:50 +0800

    Bug fixed

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,11 +1,53 @@
 const customerModel = require('../models/customerModel');
+const { v4: uuidv4 } = require('uuid');
 
 const createCustomer = async (req, res) => {
     try {
-        const result = await customerModel.createCustomer(req.body);
-        res.status(201).json(result);
+        console.log("Received customer data:", req.body);
+
+        if (!req.body || !req.body.CustomerLeadID) {
+            return res.status(400).send({ message: 'Missing required customer data' });
+        }
+
+        // Extracting customer data from the request body
+        const customerData = {
+            CustomerUUID: uuidv4(),
+            CustomerLeadID: req.body.CustomerLeadID, // Assuming this is provided in the request
+            CustomerProfile: req.body.CustomerProfile,
+            CustomerName: req.body.CustomerName,
+            CustomerEmail: req.body.CustomerEmail,
+            CustomerContactNo: req.body.CustomerContactNo,
+            CustomerICPassportNo: req.body.CustomerICPassportNo,
+            CustomerGender: req.body.CustomerGender,
+            CustomerSalutation: req.body.CustomerSalutation,
+            CustomerOccupation: req.body.CustomerOccupation,
+            CustomerNationality: req.body.CustomerNationality,
+            CustomerAddress: req.body.CustomerAddress,
+            CustomerAddress2: req.body.CustomerAddress2, // Assuming these are optional
+            CustomerAddress3: req.body.CustomerAddress3, // Assuming these are optional
+            CustomerDateOfBirth: req.body.CustomerDateOfBirth,
+            CustomerIncome: req.body.CustomerIncome,
+            CustomerMaritalStatus: req.body.CustomerMaritalStatus,
+            CustomerRace: req.body.CustomerRace,
+            CustomerIsBumi: req.body.CustomerIsBumi,
+            CustomerIsCorporate: req.body.CustomerIsCorporate,
+            CustomerPreferredLanguage: req.body.CustomerPreferredLanguage,
+            CustomerBeneficiaryID: req.body.CustomerBeneficiaryID, // Assuming this is optional or provided
+            CustomerMotherMaidenName: req.body.CustomerMotherMaidenName, // Assuming this is optional
+            CustomerEmergencyContactID: req.body.CustomerEmergencyContactID, // Assuming this is optional
+            Remark: req.body.Remark, // Assuming this is optional
+            CreatedBy: 'Developer',
+            DateCreated: new Date(),
+            ModifiedBy: null, // or req.body.ModifiedBy, depending on your logic
+            DateModified: null, // Handle this according to your application logic
+            Deleted: 0
+        };
+
+        await customerModel.createCustomer(customerData);
+
+        res.status(201).send({ message: 'Customer created successfully', CustomerUUID: customerData.CustomerUUID });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        res.status(500).send({ message: 'Error creating customer', error: error.message });
     }
 };
 
@@ -37,12 +79,12 @@ const deleteCustomer = async (req, res) => {
 };
 
 const listAllCustomers = async (req, res) => {
-  try {
-      const customers = await customerModel.listAllCustomers();
-      res.status(200).json(customers);
-  } catch (error) {
-      res.status(500).json({ message: 'Error fetching customers', error: error.message });
-  }
+    try {
+        const customers = await customerModel.listAllCustomers();
+        res.status(200).json(customers);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching customers', error: error.message });
+    }
 };
 
 module.exports = {
--- models/customerModel.js
+++ models/customerModel.js
@@ -1,46 +1,15 @@
 const db = require('./db');
+const mysql = require('mysql2'); 
 
 async function existsInTable(table, column, value) {
     const connection = await db.connect();
     try {
-        const [rows] = await connection.execute(`SELECT COUNT(*) as count FROM ?? WHERE ?? = ?`, [table, column, value]);
+        const query = `SELECT COUNT(*) as count FROM ${mysql.escapeId(table)} WHERE ${mysql.escapeId(column)} = ?`;
+
+        const [rows] = await connection.execute(query, [value]);
         return rows[0].count > 0;
-    } finally {
-        await connection.end();
-    }
-}
-
-async function checkForeignKeyConstraints(customerData) {
-    const connection = await db.connect();
-
-    // Destructure customer data to get foreign key fields
-    const {
-        CustomerLeadID, CustomerGender, CustomerNationality, CustomerAddress,
-        CustomerAddress2, CustomerAddress3, CustomerBeneficiaryID,
-        CustomerEmergencyContactID, CustomerContactNo, CustomerEmail,
-        CustomerMaritalStatus, CustomerRace, CustomerPreferredLanguage
-    } = customerData;
-
-    try {
-        // Check each foreign key constraint
-        if (CustomerLeadID && !await existsInTable('leads', 'LeadUUID', CustomerLeadID)) return false;
-        if (CustomerGender && !await existsInTable('global_gender', 'Gender', CustomerGender)) return false;
-        if (CustomerNationality && !await existsInTable('global_country', 'Country', CustomerNationality)) return false;
-        if (CustomerAddress && !await existsInTable('global_address', 'AddressUUID', CustomerAddress)) return false;
-        if (CustomerAddress2 && !await existsInTable('global_address', 'AddressUUID', CustomerAddress2)) return false;
-        if (CustomerAddress3 && !await existsInTable('global_address', 'AddressUUID', CustomerAddress3)) return false;
-        if (CustomerBeneficiaryID && !await existsInTable('customer_beneficiary', 'BeneficiaryID', CustomerBeneficiaryID)) return false;
-        if (CustomerEmergencyContactID && !await existsInTable('customer_emergency', 'EmergencyID', CustomerEmergencyContactID)) return false;
-        if (CustomerContactNo && !await existsInTable('global_contact', 'Contact', CustomerContactNo)) return false;
-        if (CustomerEmail && !await existsInTable('global_email', 'Email', CustomerEmail)) return false;
-        if (CustomerMaritalStatus && !await existsInTable('global_marital_status', 'MaritalStatus', CustomerMaritalStatus)) return false;
-        if (CustomerRace && !await existsInTable('global_race', 'Race', CustomerRace)) return false;
-        if (CustomerPreferredLanguage && !await existsInTable('global_language', 'Language', CustomerPreferredLanguage)) return false;
-
-        // If all checks pass
-        return true;
     } catch (error) {
-        console.error('Error checking foreign key constraints:', error);
+        console.error('Error in existsInTable:', error);
         throw error;
     } finally {
         await connection.end();
@@ -48,16 +17,115 @@ async function checkForeignKeyConstraints(customerData) {
 }
 
 async function createCustomer(customerData) {
-    if (await checkForeignKeyConstraints(customerData)) {
-        const connection = await db.connect();
-        try {
-            const [result] = await connection.execute(`INSERT INTO customers SET ?`, [customerData]);
-            return result;
-        } finally {
-            await connection.end();
+    const connection = await db.connect();
+    try {
+        // First, check all foreign key constraints
+        const fkConstraintsValid = await checkForeignKeyConstraints(customerData);
+        if (!fkConstraintsValid) {
+            throw new Error('Foreign key constraint validation failed');
         }
+
+        // Then, proceed with the INSERT operation
+        const query = `
+            INSERT INTO DW_STUDENT.customers (
+                CustomerUUID,
+                CustomerLeadID,
+                CustomerProfile,
+                CustomerName,
+                CustomerEmail,
+                CustomerContactNo,
+                CustomerICPassportNo,
+                CustomerGender,
+                CustomerSalutation,
+                CustomerOccupation,
+                CustomerNationality,
+                CustomerAddress,
+                CustomerAddress2,
+                CustomerAddress3,
+                CustomerDateOfBirth,
+                CustomerIncome,
+                CustomerMaritalStatus,
+                CustomerRace,
+                CustomerIsBumi,
+                CustomerIsCorporate,
+                CustomerPreferredLanguage,
+                CustomerBeneficiaryID,
+                CustomerMotherMaidenName,
+                CustomerEmergencyContactID,
+                Remark,
+                CreatedBy,
+                DateCreated,
+                ModifiedBy,
+                DateModified,
+                Deleted
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+
+        const values = [
+            customerData.CustomerUUID,
+            customerData.CustomerLeadID,
+            customerData.CustomerProfile,
+            customerData.CustomerName,
+            customerData.CustomerEmail,
+            customerData.CustomerContactNo,
+            customerData.CustomerICPassportNo,
+            customerData.CustomerGender,
+            customerData.CustomerSalutation,
+            customerData.CustomerOccupation,
+            customerData.CustomerNationality,
+            customerData.CustomerAddress,
+            customerData.CustomerAddress2,
+            customerData.CustomerAddress3,
+            customerData.CustomerDateOfBirth,
+            customerData.CustomerIncome,
+            customerData.CustomerMaritalStatus,
+            customerData.CustomerRace,
+            customerData.CustomerIsBumi,
+            customerData.CustomerIsCorporate,
+            customerData.CustomerPreferredLanguage,
+            customerData.CustomerBeneficiaryID,
+            customerData.CustomerMotherMaidenName,
+            customerData.CustomerEmergencyContactID,
+            customerData.Remark,
+            customerData.CreatedBy,
+            customerData.DateCreated,
+            customerData.ModifiedBy,
+            customerData.DateModified,
+            customerData.Deleted
+        ];
+
+        await connection.execute(query, values);
+    } catch (error) {
+        console.error('Error in createCustomer:', error);
+        throw error;
+    } finally {
+        await connection.end();
     }
-    throw new Error('Foreign key constraint check failed');
+}
+
+async function checkForeignKeyConstraints(customerData) {
+    try {
+        // Check each foreign key constraint
+        if (customerData.CustomerLeadID && !(await existsInTable('leads', 'LeadUUID', customerData.CustomerLeadID))) return false;
+        if (customerData.CustomerGender && !(await existsInTable('global_gender', 'Gender', customerData.CustomerGender))) return false;
+        if (customerData.CustomerNationality && !(await existsInTable('global_country', 'Country', customerData.CustomerNationality))) return false;
+        if (customerData.CustomerAddress && !(await existsInTable('global_address', 'AddressUUID', customerData.CustomerAddress))) return false;
+        if (customerData.CustomerAddress2 && !(await existsInTable('global_address', 'AddressUUID', customerData.CustomerAddress2))) return false;
+        if (customerData.CustomerAddress3 && !(await existsInTable('global_address', 'AddressUUID', customerData.CustomerAddress3))) return false;
+        if (customerData.CustomerBeneficiaryID && !(await existsInTable('customer_beneficiary', 'BeneficiaryID', customerData.CustomerBeneficiaryID))) return false;
+        if (customerData.CustomerEmergencyContactID && !(await existsInTable('customer_emergency', 'EmergencyID', customerData.CustomerEmergencyContactID))) return false;
+        if (customerData.CustomerContactNo && !(await existsInTable('global_contact', 'Contact', customerData.CustomerContactNo))) return false;
+        if (customerData.CustomerEmail && !(await existsInTable('global_email', 'Email', customerData.CustomerEmail))) return false;
+        if (customerData.CustomerMaritalStatus && !(await existsInTable('global_marital_status', 'MaritalStatus', customerData.CustomerMaritalStatus))) return false;
+        if (customerData.CustomerRace && !(await existsInTable('global_race', 'Race', customerData.CustomerRace))) return false;
+        if (customerData.CustomerPreferredLanguage && !(await existsInTable('global_language', 'Language', customerData.CustomerPreferredLanguage))) return false;
+
+        // If all checks pass
+        return true;
+    } catch (error) {
+        console.error('Error checking foreign key constraints:', error);
+        throw error;
+    }
 }
 
 async function getCustomer(customerId) {
@@ -73,8 +141,16 @@ async function getCustomer(customerId) {
 async function updateCustomer(customerId, customerData) {
     const connection = await db.connect();
     try {
-        const [result] = await connection.execute(`UPDATE customers SET ? WHERE CustomerUUID = ?`, [customerData, customerId]);
+        const updateColumns = Object.keys(customerData).map(column => `${column} = ?`).join(', ');
+        const updateValues = Object.values(customerData);
+
+        const sql = `UPDATE customers SET ${updateColumns} WHERE CustomerUUID = ?`;
+        const [result] = await connection.execute(sql, [...updateValues, customerId]);
+
         return result;
+    } catch (error) {
+        console.error('Error updating customer:', error);
+        throw error;
     } finally {
         await connection.end();
     }
@@ -85,6 +161,9 @@ async function deleteCustomer(customerId) {
     try {
         const [result] = await connection.execute(`DELETE FROM customers WHERE CustomerUUID = ?`, [customerId]);
         return result;
+    } catch (error) {
+        console.error('Error deleting customer:', error);
+        throw error;
     } finally {
         await connection.end();
     }
