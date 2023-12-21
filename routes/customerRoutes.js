0acc431cbcd49e201fc610dfbf35ef5c54018c1a
jiahongg <88899688+jiahongg@users.noreply.github.com>
2023-12-22 07:10:54 +0800

    Bug fixed

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
-const { verifyToken, checkApiAccess } = require('../middleware/authMiddleware');
+const { verifyToken, checkAccess, checkApiAccess } = require('../middleware/authMiddleware');
 
+// Use verifyToken for all routes in this router
 router.use(verifyToken);
 
+// Use checkApiAccess middleware for '/customers' route
 router.use(checkApiAccess('customers'));
 
 router.route('/')
-  .post(customerController.createCustomer)
+  .post(checkAccess('editor'), customerController.createCustomer)
   .get(customerController.listAllCustomers);
 
 router.route('/:id')
   .get(customerController.getCustomer)
-  .put(customerController.updateCustomer)
-  .delete(customerController.deleteCustomer);
+  .put(checkAccess('editor'), customerController.updateCustomer)
+  .delete(checkAccess('admin'), customerController.deleteCustomer);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const userApiController = require('../controllers/userApiController');
-const { verifyToken, checkAccess, checkApiAccess } = require('../middleware/authMiddleware');
+const { verifyToken, checkAccess } = require('../middleware/authMiddleware');
 
 const router = express.Router();
+
+// Middleware for all user routes
+router.use(verifyToken);
 
 router.post('/login', authController.loginRateLimiter, authController.login);
 router.post('/refresh', authController.refreshAccessToken);
 router.post('/logout', authController.logout);
 
+// Routes with common middleware
 router.route('/')
-  .post(verifyToken, checkAccess(['admin']), userApiController.createUser)
-  .get(verifyToken, checkAccess(['admin', 'editor']), userApiController.listUsers);
+  .post(checkAccess(['admin']), userApiController.createUser)
+  .get(checkAccess(['admin', 'editor']), userApiController.listUsers);
 
 router.route('/:id')
-  .get(verifyToken, checkAccess(['admin', 'editor']), userApiController.getUserDetails)
-  .put(verifyToken, checkAccess(['admin', 'editor']), userApiController.updateUser)
-  .delete(verifyToken, checkAccess(['admin']), userApiController.deleteUser);
+  .get(checkAccess(['admin', 'editor']), userApiController.getUserDetails)
+  .put(checkAccess(['admin', 'editor']), userApiController.updateUser)
+  .delete(checkAccess(['admin']), userApiController.deleteUser);
 
 router.route('/apiCollectionGroups')
-  .post(verifyToken, checkAccess(['admin']), userApiController.createApiCollectionGroup);
-  // .get(verifyToken, checkAccess(['admin', 'editor']), userApiController.listApiCollectionGroups);
+  .post(checkAccess(['admin']), userApiController.createApiCollectionGroup);
+  // .get(checkAccess(['admin', 'editor']), userApiController.listApiCollectionGroups);
 
 router.route('/userApiCollectionGroup')
-  .post(verifyToken, checkAccess(['admin']), userApiController.assignApiCollectionGroupToUser);
+  .post(checkAccess(['admin']), userApiController.assignApiCollectionGroupToUser);
 
 router.route('/userApiCollectionGroup/:userUUID')
-  .get(verifyToken, checkAccess(['admin', 'editor']), userApiController.listUserApiCollectionGroups);
+  .get(checkAccess(['admin', 'editor']), userApiController.listUserApiCollectionGroups);
 
 module.exports = router;
